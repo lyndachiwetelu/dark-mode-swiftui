c3690a558924bee01307cf0c75477ce56c607201
Lynda Chiwetelu <lyndachiwe@gmail.com>
2023-08-27 23:33:51 +0200

    initial commit - dark mode support

--- DarkMode/ContentView.swift
+++ DarkMode/ContentView.swift
@@ -7,15 +7,87 @@
 
 import SwiftUI
 
+struct Reminder {
+    let id: Int
+    let title: String
+    let time: String
+}
+
 struct ContentView: View {
+    @Environment(\.colorScheme) private var userColorScheme
+    
+    private var reminders: [Reminder] = [
+        Reminder(id:1, title: "Walk the dog", time: "14 mins"),
+        Reminder(id: 2, title: "Bake a marble cake", time: "20 mins"),
+        Reminder(id: 3, title: "Make an app", time: "1 hr"),
+        Reminder(id: 4, title: "Clean the room", time: "1.5 hrs"),
+    ]
+    
+    @State private var viewColorScheme: ColorScheme = .light
+    
+    @State private var isOn: Bool = false
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Toggle("", isOn: $isOn).tint(Color("AppAccentBlue"))
+            
+            Text("Upcoming Reminders")
+                .font(.title3)
+                .foregroundColor(Color("AppNavyBlue"))
+                .padding(.bottom, 30)
+            
+            VStack {
+                ForEach(reminders, id:\.id) { reminder in
+                    HStack {
+                        Text(reminder.title).foregroundColor(Color(UIColor.label))
+                        Spacer()
+                        Text(reminder.time).foregroundColor(Color(UIColor.label))
+                    }.padding(.all, 20)
+                        .background(Color("AppBlue"))
+                        .cornerRadius(8)
+                        .padding(.bottom, 5)
+                }
+            }
+            
+            Text("Completed Reminders")
+                .font(.title3)
+                .foregroundColor(Color("AppNavyBlue"))
+                .padding(.bottom, 20).padding(.top, 30)
+            
+            VStack {
+                Image("no-tasks")
+                    .resizable()
+                    .frame(width: 100, height: 100)
+                    .padding(.bottom, 10)
+                Text("You have no completed tasks yet").foregroundColor(.gray)
+            }
+            
+            
+            Spacer()
+            
+        }.preferredColorScheme(viewColorScheme)
+        .onAppear() {
+            switchAppearance()
+        }.onChange(of: isOn, perform: { newValue in
+            if newValue == false {
+                viewColorScheme = .dark
+                return
+            }
+            
+            viewColorScheme = .light
+        })
+        
+        .padding(.all, 20)
+    }
+    
+    func switchAppearance() {
+        viewColorScheme = userColorScheme
+        if viewColorScheme == .light {
+            isOn = true
+            return
         }
-        .padding()
+        
+        isOn = false
     }
 }
 
