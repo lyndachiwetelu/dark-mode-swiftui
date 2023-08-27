a48f7c785e792c9eac0049e131f305d6744f6653
Lynda Chiwetelu <lyndachiwe@gmail.com>
2023-08-27 16:04:23 +0200

    Initial Commit

---
+++ DarkMode/DarkModeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DarkModeApp.swift
+//  DarkMode
+//
+//  Created by Lynda Chiwetelu on 27.08.23.
+//
+
+import SwiftUI
+
+@main
+struct DarkModeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
